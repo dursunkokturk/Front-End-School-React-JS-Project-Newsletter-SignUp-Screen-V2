4c52678da5698349c280cb3eb5c4b26e30a69128
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-04-15 09:23:38 +0300

    src klasoru icindeki assets klasoru icindeki components klasoru icinde yer alan newslettersignupscreenwrite.jsx component'inda haber bulteni faydasiyla ilgili yazilar girildi, kullanicindan email almak icin gereken input ve buton eklendi

--- src/assets/Components/NewsletterSignupScreenWrite.jsx
+++ src/assets/Components/NewsletterSignupScreenWrite.jsx
@@ -6,7 +6,23 @@ export default function NewsletterSignupScreenWrite() {
     <main>
       <h1>Gelişmeleri takip edin!</h1>
       <h4>Aşağıdaki konularda aylık güncellemeler alan 60.000'den fazla ürün yöneticisine katılın:</h4>
-      <img src={Oval} className="oval" alt="resim" />
+      <div className="check-and-write">
+        <img src={Oval} className="oval" alt="resim" />
+        <p>Ürün keşfi ve gerçekten önemli olanı yaratmak</p>
+      </div>
+      <div className="check-and-write">
+        <img src={Oval} className="oval" alt="resim" />
+        <p>Güncellemelerin başarılı olduğundan emin olmak için ölçüm yapmak</p>
+      </div>
+      <div className="check-and-write">
+        <img src={Oval} className="oval" alt="resim" />
+        <p className="third">Ve daha fazlası!</p>
+      </div>
+      <div className="email">
+        <p>E-posta adresi</p>
+        <input type="email" name="" id="" />
+        <button>Aylık bültene abone olun</button>
+      </div>
     </main>
   )
 }
