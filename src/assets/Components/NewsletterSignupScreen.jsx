ab70e24e699b915a764e03d6224a3de4e64f7eef
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-04-16 16:44:46 +0300

    src klasoru icindeki assets klasoru icindeki components klasoru icinde yer alan newslettersignupscreen.jsx component'i icindeki tasarimlarin desktop gorunumlerinde kullanilanlari eklendi, tasarimlarin mobil gorunume uygun olanlari mobil ve tablet ekranda gosterilecek, desktop gorunume uygun olanlari desktop gornumde gosterilecek, desktop gorunum icin css duzenlemesi yapilacak

--- src/assets/Components/NewsletterSignupScreen.jsx
+++ src/assets/Components/NewsletterSignupScreen.jsx
@@ -1,9 +1,15 @@
 import Background from "../img/background.png"
+import BackgroundBig from "../img/background-big.png"
 import Tablet from "../img/tablet.png"
+import TabletBig from "../img/tablet-big.png"
 import ComputerScreen from "../img/computer-screen.png"
+import ComputerScreenBig from "../img/computer-screen-big.png"
 import Grafic from "../img/grafic.png"
+import GraficBig from "../img/grafic-big.png"
 import Ellipse from "../img/ellipse.png"
+import EllipseBig from "../img/ellipse-big.png"
 import Number from "../img/number.png"
+import NumberBig from "../img/number-big.png"
 import Oval from "../img/oval.png"
 import "../../App"
 
@@ -12,12 +18,19 @@ export default function NewsletterSignupScreen({ setSubscription }) {
     <>
       <header>
         <img src={Number} className="number" alt="resim" />
+        <img src={NumberBig} className="number-big" alt="resim" />
         <div className="white-background"></div>
+        <div className="white-background-big"></div>
         <img src={Ellipse} className="ellipse" alt="resim" />
+        <img src={EllipseBig} className="ellipse-big" alt="resim" />
         <img src={Grafic} className="grafic" alt="resim" />
+        <img src={GraficBig} className="grafic-big" alt="resim" />
         <img src={ComputerScreen} className="computer-screen" alt="resim" />
+        <img src={ComputerScreenBig} className="computer-screen-big" alt="resim" />
         <img src={Tablet} className="tablet" alt="resim" />
+        <img src={TabletBig} className="tablet-big" alt="resim" />
         <img src={Background} className="background" alt="resim" />
+        <img src={BackgroundBig} className="background-big" alt="resim" />
       </header>
       <main>
         <h1>Gelişmeleri takip edin!</h1>
