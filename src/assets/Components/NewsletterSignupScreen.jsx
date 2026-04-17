57b4ede1e3549b6a895b29b25f5375ff2b1bc7a9
Dursun Köktürk <dursunserefkokturk@gmail.com>
2026-04-17 11:42:31 +0300

    src klasoru icindeki assets klasoru icindeki components klasoru icinde yer alan newslettersignupscreen.jsx component'i icindeki tasarimlarin desktop gorunumleri icin header tag'inda css duzenlemesi yapildi, header-and-main div'i icinde background-color verildi css duzenlemesi yapildi, body icinde background-color verildi

--- src/assets/Components/NewsletterSignupScreen.jsx
+++ src/assets/Components/NewsletterSignupScreen.jsx
@@ -16,45 +16,47 @@ import "../../App"
 export default function NewsletterSignupScreen({ setSubscription }) {
   return (
     <>
-      <header>
-        <img src={Number} className="number" alt="resim" />
-        <img src={NumberBig} className="number-big" alt="resim" />
-        <div className="white-background"></div>
-        <div className="white-background-big"></div>
-        <img src={Ellipse} className="ellipse" alt="resim" />
-        <img src={EllipseBig} className="ellipse-big" alt="resim" />
-        <img src={Grafic} className="grafic" alt="resim" />
-        <img src={GraficBig} className="grafic-big" alt="resim" />
-        <img src={ComputerScreen} className="computer-screen" alt="resim" />
-        <img src={ComputerScreenBig} className="computer-screen-big" alt="resim" />
-        <img src={Tablet} className="tablet" alt="resim" />
-        <img src={TabletBig} className="tablet-big" alt="resim" />
-        <img src={Background} className="background" alt="resim" />
-        <img src={BackgroundBig} className="background-big" alt="resim" />
-      </header>
-      <main>
-        <h1>Gelişmeleri takip edin!</h1>
-        <h4>Aşağıdaki konularda aylık güncellemeler alan 60.000'den fazla ürün yöneticisine katılın:</h4>
-        <div className="check-and-write">
-          <img src={Oval} className="oval" alt="resim" />
-          <p>Ürün keşfi ve gerçekten önemli olanı yaratmak</p>
-        </div>
-        <div className="check-and-write">
-          <img src={Oval} className="oval" alt="resim" />
-          <p>Güncellemelerin başarılı olduğundan emin olmak için ölçüm yapmak</p>
-        </div>
-        <div className="check-and-write">
-          <img src={Oval} className="oval" alt="resim" />
-          <p className="third">Ve daha fazlası!</p>
-        </div>
-        <div className="email">
-          <p>E-posta adresi</p>
-          <input type="email" name="" id="" />
-          <button onClick={() => setSubscription(true)}>
-            Aylık bültene abone olun
-          </button>
-        </div>
-      </main>
+      <div className="header-and-main">
+        <header>
+          <img src={Number} className="number" alt="resim" />
+          <img src={NumberBig} className="number-big" alt="resim" />
+          <div className="white-background"></div>
+          <div className="white-background-big"></div>
+          <img src={Ellipse} className="ellipse" alt="resim" />
+          <img src={EllipseBig} className="ellipse-big" alt="resim" />
+          <img src={Grafic} className="grafic" alt="resim" />
+          <img src={GraficBig} className="grafic-big" alt="resim" />
+          <img src={ComputerScreen} className="computer-screen" alt="resim" />
+          <img src={ComputerScreenBig} className="computer-screen-big" alt="resim" />
+          <img src={Tablet} className="tablet" alt="resim" />
+          <img src={TabletBig} className="tablet-big" alt="resim" />
+          <img src={Background} className="background" alt="resim" />
+          <img src={BackgroundBig} className="background-big" alt="resim" />
+        </header>
+        <main>
+          <h1>Gelişmeleri takip edin!</h1>
+          <h4>Aşağıdaki konularda aylık güncellemeler alan 60.000'den fazla ürün yöneticisine katılın:</h4>
+          <div className="check-and-write">
+            <img src={Oval} className="oval" alt="resim" />
+            <p>Ürün keşfi ve gerçekten önemli olanı yaratmak</p>
+          </div>
+          <div className="check-and-write">
+            <img src={Oval} className="oval" alt="resim" />
+            <p>Güncellemelerin başarılı olduğundan emin olmak için ölçüm yapmak</p>
+          </div>
+          <div className="check-and-write">
+            <img src={Oval} className="oval" alt="resim" />
+            <p className="third">Ve daha fazlası!</p>
+          </div>
+          <div className="email">
+            <p>E-posta adresi</p>
+            <input type="email" name="" id="" />
+            <button onClick={() => setSubscription(true)}>
+              Aylık bültene abone olun
+            </button>
+          </div>
+        </main>
+      </div>
     </>
   )
 }
